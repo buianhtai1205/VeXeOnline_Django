10acd3dfffff097e83f4dd25286709a7a05a2c38
buianhtai1205 <buianhtai2017tq@gmail.com>
2022-11-28 23:17:32 +0700

    update managers.models

--- customers/views.py
+++ customers/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from managers.models import Trip
+
+
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def index(request):
-    return render(request, 'customers/index.html')
+    departure = Trip.objects.distinct().values('departure')
+    destination = Trip.objects.distinct().values('destination')
+    return render(request, 'customers/index.html', {'departure': departure, 'destination': destination})
 
 def listCoach(request):
-    return render(request, 'customers/listCoach.html')
+    if request.method == 'POST':
+        data = request.POST.dict()
+        departure = data['departure']
+        destination = data['destination']
+        departure_time = data['departure_time']
+    list_trip = Trip.objects.all().filter(departure = departure, destination = destination, departure_time__date = departure_time).prefetch_related()
+    # data = serializers.serialize('json', list_trip)
+    # return HttpResponse(data, content_type='application/json')
+    return render(request, 'customers/listCoach.html', {'list_trip': list_trip})  
